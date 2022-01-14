4bc70fe0940e73e4659d1bb99d78e2f231ea5cdc
andres-castiblanco <37271641+andres-castiblanco@users.noreply.github.com>
2022-01-13 23:22:55 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,104 @@
+function clicked() {
+
+	var user = document.getElementById('username');
+	var pass = document.getElementById('password');
+	
+	var coruser = "Quindio";
+	var corpass = "200514";
+	
+	if(user.value == coruser) {
+	
+		if(pass.value == corpass) {
+		
+			/*window.alert("You are logged in as " + user.value);*/
+			window.open("https://andres-castiblanco.github.io/Quindio/","_self");
+		
+		} else {
+		
+			window.alert("Usuario o clave incorrecta!, Vuelva a intentar");
+		
+		}
+
+	
+	} 
+
+	var coruser1 = "FExE";
+	var corpass1 = "011220";
+
+	if(user.value == coruser1) {
+	
+		if(pass.value == corpass1) {
+		
+			/*window.alert("You are logged in as " + user.value);*/
+			window.open("https://andres-castiblanco.github.io/FExE/","_self");
+		
+		} else {
+		
+			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+		
+		}
+	
+	} 
+
+	var coruser2 = "CV";
+	var corpass2 = "012345";
+
+	if(user.value == coruser2) {
+	
+		if(pass.value == corpass2) {
+		
+			/*window.alert("You are logged in as " + user.value);*/
+			window.open("https://andres-castiblanco.github.io/CV/","_self");
+		
+		} else {
+		
+			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+		
+		}
+	
+	} 
+
+	var coruser3 = "---";
+	var corpass3 = "---";
+
+	if(user.value == coruser3) {
+	
+		if(pass.value == corpass3) {
+		
+			/*window.alert("You are logged in as " + user.value);*/
+			window.open("https://google.com/","_self");
+		
+		} else {
+		
+			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+		
+		}
+	
+	} 
+
+	var coruser4 = "----";
+	var corpass4 = "----";
+
+	if(user.value == coruser4) {
+	
+		if(pass.value == corpass4) {
+		
+			/*window.alert("You are logged in as " + user.value);*/
+			window.open("https://google.com/","_self");
+		
+		} else {
+		
+			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+		
+		}
+	
+	} 
+
+	else {
+		
+			window.alert("Incorrect username or password!");
+		
+		}
+
+}
+
