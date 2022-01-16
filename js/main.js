5be51456bc9fba3686d4724d8296eaea2fecaaad
andres-castiblanco <37271641+andres-castiblanco@users.noreply.github.com>
2022-01-15 22:40:59 -0500

    update

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@ function clicked() {
 	var user = document.getElementById('username');
 	var pass = document.getElementById('password');
 	
-	var coruser = "Quindio";
-	var corpass = "200514";
+
+	/* Equipo Nacional */
+		var coruser = "alvaro.castro";
+		var corpass = "400297";
 	
 	if(user.value == coruser) {
 	
 		if(pass.value == corpass) {
 		
 			/*window.alert("You are logged in as " + user.value);*/
-			window.open("https://andres-castiblanco.github.io/Quindio/","_self");
+			window.open("https://andres-castiblanco.github.io/SF-2022","_self");
 		
 		} else {
 		
@@ -22,81 +24,81 @@ function clicked() {
 	
 	} 
 
-	var coruser1 = "FExE";
-	var corpass1 = "011220";
+		var coruser1 = "camilo.enriquez";
+		var corpass1 = "585694";
 
-	if(user.value == coruser1) {
-	
-		if(pass.value == corpass1) {
+		if(user.value == coruser1) {
 		
-			/*window.alert("You are logged in as " + user.value);*/
-			window.open("https://andres-castiblanco.github.io/FExE/","_self");
+			if(pass.value == corpass1) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://andres-castiblanco.github.io/FExE/","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
 		
-		} else {
-		
-			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
-		
-		}
-	
-	} 
+		} 
 
-	var coruser2 = "CV";
-	var corpass2 = "012345";
+		var coruser2 = "CV";
+		var corpass2 = "012345";
 
-	if(user.value == coruser2) {
-	
-		if(pass.value == corpass2) {
+		if(user.value == coruser2) {
 		
-			/*window.alert("You are logged in as " + user.value);*/
-			window.open("https://andres-castiblanco.github.io/CV/","_self");
+			if(pass.value == corpass2) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://andres-castiblanco.github.io/CV/","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
 		
-		} else {
-		
-			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
-		
-		}
-	
-	} 
+		} 
 
-	var coruser3 = "---";
-	var corpass3 = "---";
+		var coruser3 = "---";
+		var corpass3 = "---";
 
-	if(user.value == coruser3) {
-	
-		if(pass.value == corpass3) {
+		if(user.value == coruser3) {
 		
-			/*window.alert("You are logged in as " + user.value);*/
-			window.open("https://google.com/","_self");
+			if(pass.value == corpass3) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://google.com/","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
 		
-		} else {
-		
-			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
-		
-		}
-	
-	} 
+		} 
 
-	var coruser4 = "----";
-	var corpass4 = "----";
+		var coruser4 = "----";
+		var corpass4 = "----";
 
-	if(user.value == coruser4) {
-	
-		if(pass.value == corpass4) {
+		if(user.value == coruser4) {
 		
-			/*window.alert("You are logged in as " + user.value);*/
-			window.open("https://google.com/","_self");
+			if(pass.value == corpass4) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://google.com/","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
 		
-		} else {
-		
-			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
-		
-		}
-	
-	} 
+		} 
 
 	else {
 		
-			/*window.alert("Incorrect username or password!");*/
+			window.alert("Incorrect username or password!");
 		
 		}
 
