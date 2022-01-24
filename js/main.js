82a1b78f6a1f79ccab5d143d3df9a55f04673db0
andres-castiblanco <37271641+andres-castiblanco@users.noreply.github.com>
2022-01-23 21:14:38 -0500

    update

--- js/main.js
+++ js/main.js
@@ -42,12 +42,30 @@ function clicked() {
 		
 		} 
 
-		var coruser2 = "CV";
-		var corpass2 = "012345";
+		var coruser2 = "camilo.lema";
+		var corpass2 = "758493";
 
 		if(user.value == coruser2) {
 		
 			if(pass.value == corpass2) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://andres-castiblanco.github.io/SF-2022/vallas.html","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
+		
+		}
+
+		var coruser3 = "CV";
+		var corpass3 = "012345";
+
+		if(user.value == coruser3) {
+		
+			if(pass.value == corpass3) {
 			
 				/*window.alert("You are logged in as " + user.value);*/
 				window.open("https://app.powerbi.com/view?r=eyJrIjoiYjk0NGY1YWEtZGNkZi00NTU4LWE5ZjctZDY4ZDY5YjgwNDMwIiwidCI6IjU3N2ZjMWQ4LTA5MjItNDU4ZS04N2JmLWVjNGY0NTVlYjYwMCIsImMiOjR9&pageName=ReportSection","_self");
@@ -60,12 +78,12 @@ function clicked() {
 		
 		} 
 
-		var coruser3 = "---";
-		var corpass3 = "---";
+		var coruser4 = "---";
+		var corpass4 = "---";
 
-		if(user.value == coruser3) {
+		if(user.value == coruser4) {
 		
-			if(pass.value == corpass3) {
+			if(pass.value == corpass4) {
 			
 				/*window.alert("You are logged in as " + user.value);*/
 				window.open("https://google.com/","_self");
@@ -78,12 +96,12 @@ function clicked() {
 		
 		} 
 
-		var coruser4 = "----";
-		var corpass4 = "----";
+		var coruser5 = "----";
+		var corpass5 = "----";
 
-		if(user.value == coruser4) {
+		if(user.value == coruser5) {
 		
-			if(pass.value == corpass4) {
+			if(pass.value == corpass5) {
 			
 				/*window.alert("You are logged in as " + user.value);*/
 				window.open("https://google.com/","_self");
