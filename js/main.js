587b46c3a4d7abc70955de5429530765e7b9eb1b
andres-castiblanco <37271641+andres-castiblanco@users.noreply.github.com>
2022-02-04 00:17:12 -0500

    update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function clicked() {
 	var pass = document.getElementById('password');
 	
 
-	/* Equipo Nacional */
+	/* Equipo Nacional  - Comite Estrategico  Alvaro, Camilo Lema, Luis H, Sara, Johanna Peters */
 		var coruser = "alvaro.castro";
 		var corpass = "400297";
 	
@@ -13,77 +13,154 @@ function clicked() {
 		if(pass.value == corpass) {
 		
 			/*window.alert("You are logged in as " + user.value);*/
-			window.open("https://andres-castiblanco.github.io/SF-2022/11001.html","_self");
+			window.open("https://andres-castiblanco.github.io/SF-2022/EquipoNacional.html","_self");
 	
 		} else {
 		
 			window.alert("Usuario o clave incorrecta!, Vuelva a intentar");
 		
 		}
-
-	
 	} 
+	
+	/* Equipo Nacional  - Comite Estrategico */
+		var coruser1 = "camilo.lema";
+		var corpass1 = "758493";
 
-		var coruser1 = "camilo.enriquez";
-		var corpass1 = "585694";
-
-		if(user.value == coruser1) {
+	if(user.value == coruser1) {
+	
+		if(pass.value == corpass1) {
 		
-			if(pass.value == corpass1) {
-			
-				/*window.alert("You are logged in as " + user.value);*/
-				window.open("https://andres-castiblanco.github.io/SF-2022/11001.html","_self");
-			
-			} else {
-			
-				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
-			
-			}
+			/*window.alert("You are logged in as " + user.value);*/
+			window.open("https://andres-castiblanco.github.io/SF-2022/vallas.html","_self");
 		
-		} 
-
-		var coruser2 = "camilo.lema";
-		var corpass2 = "758493";
-
-		if(user.value == coruser2) {
+		} else {
 		
-			if(pass.value == corpass2) {
-			
-				/*window.alert("You are logged in as " + user.value);*/
-				window.open("https://andres-castiblanco.github.io/SF-2022/vallas.html","_self");
-			
-			} else {
-			
-				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
-			
-			}
+			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
 		
 		}
+	}
 
-		var coruser3 = "CV";
-		var corpass3 = "012345";
+	/* Equipo Nacional  - Comite Estrategico */
+		var coruser2 = "luis.berrio";
+		var corpass2 = "----";
 
-		if(user.value == coruser3) {
-		
-			if(pass.value == corpass3) {
+	if(user.value == coruser2) {
+
+		if(pass.value == corpass2) {
 			
-				/*window.alert("You are logged in as " + user.value);*/
-				window.open("https://app.powerbi.com/view?r=eyJrIjoiYjk0NGY1YWEtZGNkZi00NTU4LWE5ZjctZDY4ZDY5YjgwNDMwIiwidCI6IjU3N2ZjMWQ4LTA5MjItNDU4ZS04N2JmLWVjNGY0NTVlYjYwMCIsImMiOjR9&pageName=ReportSection","_self");
+			/*window.alert("You are logged in as " + user.value);*/
+			window.open("https://andres-castiblanco.github.io/SF-2022/EquipoNacional.html","_self");
 			
-			} else {
+		} else {
 			
-				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
 			
 			}
-		
-		} 
 
-		var coruser4 = "---";
-		var corpass4 = "---";
+		}
+
+	/* Equipo Nacional  - Comite Estrategico */
+		var coruser3 = "sara.moreno";
+		var corpass3 = "---";
+
+	if(user.value == coruser3) {
+
+		if(pass.value == corpass3) {
+
+			/*window.alert("You are logged in as " + user.value);*/
+			window.open("https://andres-castiblanco.github.io/SF-2022/EquipoNacional.html","_self");
+
+		} else {
+
+			window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+
+		}
+
+	}
+
+	/* Equipo Regional - Bogota */
+		var coruser4 = "camilo.enriquez";
+		var corpass4 = "585694";
 
 		if(user.value == coruser4) {
 		
 			if(pass.value == corpass4) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://andres-castiblanco.github.io/SF-2022/Bogota.html","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
+		
+		} 
+
+	/* Equipo Regional - Antioquia */
+		var coruser5 = "----";
+		var corpass5 = "----";
+
+		if(user.value == coruser5) {
+		
+			if(pass.value == corpass5) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://andres-castiblanco.github.io/SF-2022/Antioquia.html","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
+		
+		} 
+	
+	/* Equipo Regional - Pacifico - Cali */
+		var coruser6 = "---";
+		var corpass6 = "---";
+
+		if(user.value == coruser6) {
+		
+			if(pass.value == corpass6) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://andres-castiblanco.github.io/SF-2022/Pacifico.html","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
+		
+		} 
+
+	/* Equipo Regional - Caribe */
+		var coruser7 = "---";
+		var corpass7 = "---";
+
+		if(user.value == coruser7) {
+		
+			if(pass.value == corpass7) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://andres-castiblanco.github.io/SF-2022/Caribe.html","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
+		
+		} 
+
+	/* Equipo Regional - Eje Cafetero */
+		var coruser8 = "---";
+		var corpass8 = "---";
+
+		if(user.value == coruser8) {
+		
+			if(pass.value == corpass8) {
 			
 				/*window.alert("You are logged in as " + user.value);*/
 				window.open("https://google.com/","_self");
@@ -95,6 +172,28 @@ function clicked() {
 			}
 		
 		} 
+
+	/* Equipo Regional - Boyaca Cundinamarca */
+		var coruser9 = "---";
+		var corpass9 = "---";
+
+		if(user.value == coruser9) {
+		
+			if(pass.value == corpass9) {
+			
+				/*window.alert("You are logged in as " + user.value);*/
+				window.open("https://google.com/","_self");
+			
+			} else {
+			
+				window.alert("Usuario o clave incorrecta!, Vuelva a intentar!");
+			
+			}
+		
+		} 
+
+
+
 
 		var coruser5 = "----";
 		var corpass5 = "----";
